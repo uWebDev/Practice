b24026768234d47ab9b0ad808246185919892079
uWebDev <web.developer.job@yandex.ru>
2016-04-22 08:14:45 +0300

    Requests for CREATE and DROP tables

--- mysql/db.sql
+++ mysql/db.sql
@@ -7,3 +7,32 @@ CREATE DATABASE IF NOT EXISTS `dore`;
 
 -- Использовать базу данных
 USE `dore`;
+
+
+
+-- Создание таблицы Города
+CREATE TABLE IF NOT EXISTS `dore`.`cities` (
+  `id` INT UNSIGNED NOT NULL COMMENT '',
+  `city` VARCHAR(255) NOT NULL COMMENT '',
+  `countries_id` INT UNSIGNED NOT NULL COMMENT '',
+  UNIQUE INDEX `id` (`id`)  COMMENT '',
+  INDEX `fk_cities_countries_idx` (`countries_id`)  COMMENT '')
+ENGINE = MyISAM
+COLLATE = utf8_general_ci;
+
+-- Удаление таблицы Города
+#DROP TABLE IF EXISTS `cities`;
+
+
+
+-- Создание таблицы Страны
+CREATE TABLE IF NOT EXISTS `dore`.`countries` (
+  `id` INT UNSIGNED NOT NULL COMMENT '',
+  `name` VARCHAR(255) NOT NULL COMMENT '',
+  `currency_code` VARCHAR(5) NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '')
+ENGINE = MyISAM
+COLLATE = utf8_general_ci;
+
+-- Удаление таблицы Страны
+#DROP TABLE IF EXISTS `countries`;
